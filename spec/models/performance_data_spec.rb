1c5c213553f9d23cdae5c79e6535689be5a53e4e
MikaelFeher <mikael.feher@gmail.com>
2016-08-13 15:38:42 +0200

    setup and testing performance data model

--- spec/models/performance_data_spec.rb
+++ spec/models/performance_data_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PerformanceData, type: :model do
+
   describe 'Database table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :data }
